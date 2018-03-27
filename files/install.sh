4be62e6a2ad524e58c49f15c61c27d1d8e5bf0de
Gonçalo Almeida <gv.almeida@campus.fct.unl.pt>
2018-03-27 15:14:36 +0100

    fixed merging issues during installation

--- files/install.sh
+++ files/install.sh
@@ -1,1 +1,20 @@
-tar cf - piratebox | (cd /opt/; tar xf - ) && rm -r "./piratebox" && rm "./install.sh"
+# get directory where script is located
+FULLDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# copy over share directory first, since it might be installed in the SD card or USB stick (USB untested)
+cd "$FULLDIR/piratebox/share"
+find -type f -print0 | sudo xargs -0 -n 1 -I {} mv '{}' '/opt/piratebox/share/{}'
+cd ../..
+
+# copy over all other files
+cp -rlf "$FULLDIR/piratebox" /opt/ 
+
+# create Videos, Images and Books directories
+mkdir /opt/piratebox/share/Shared/Videos
+mkdir /opt/piratebox/share/Shared/Images
+mkdir /opt/piratebox/share/Shared/Books
+
+# clean up
+DIR="${PWD##*/}"
+cd "$FULLDIR/.."
+rmdir -r $DIR
